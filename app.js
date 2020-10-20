fee9bdcefe4b603598f7c8d40b1bc243016c4e7e
ayoung19 <zeusandninja@gmail.com>
2020-10-19 18:34:59 -0700

    add test route

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = 3000;
 
 var robot = require("robotjs");
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
     if(typeof req.body.sku == "string" && typeof req.body.size == "string") {
         robot.typeString("asd");
         robot.typeString(req.body.sku);
@@ -26,6 +26,10 @@ app.post('/', (req, res) => {
     } else {
         res.status(400).json( {message: `Incorrect Params`} )
     }
+});
+
+app.post("/test", (req, res) => {
+    res.status(200).json({ message: `Success` });
 });
 
 app.listen(PORT, () => {
